a69bae34c3945a895b8a607adc5063ed5ac38e8c
infernalslam <infernal-slam@hotmail.com>
2017-05-06 17:28:24 +0700

    function suceess

--- src/App.js
+++ src/App.js
@@ -5,6 +5,16 @@ import Feed from './components/feed'
 import Report from './components/report'
 
 class App extends Component {
+  constructor () {
+    super ()
+    this.state = {
+      data: []
+    }
+    this.data = this.addData.bind(this)
+  }
+  addData (data) {
+    console.log('data :', data)
+  }
   render() {
     return (
       <div>
@@ -12,7 +22,7 @@ class App extends Component {
         <section className="section">
           <div className="container">
             <div className="columns">
-              <div className="column"><Form /></div>
+              <div className="column"><Form addData={this.addData} /></div>
               <div className="column"><Feed /></div>
               <div className="column"><Report /></div>
             </div>
--- src/components/form.jsx
+++ src/components/form.jsx
@@ -1,9 +1,55 @@
 import React, {Component} from 'react'
 
 class Form extends Component {
+    constructor (props) {
+        super (props)
+        this.state = {
+            com: '',
+            email: '',
+            detail: ''
+        }
+        this.addData = this.props.addData.bind(this)
+        this.com = this.com.bind(this)
+        this.email = this.email.bind(this)
+        this.detail = this.detail.bind(this)
+    }
+    com (e) {
+        this.setState({
+            com: e.target.value
+        })
+    }
+    email (e) {
+        this.setState({
+            email: e.target.value
+        })
+    }
+
+    detail (e) {
+        this.setState({
+            detail: e.target.value
+        })
+    }
+
     render () {
         return (
-            <h1> form!! </h1>
+        <div>
+            <div className="field">
+            <p className="control">
+            <input className="input" type="text" placeholder="บริษัท"  onChange={this.com} />
+            </p>
+            </div>
+            <div className="field">
+                <p className="control">
+                  <input className="input" type="email" placeholder="Email" onChange={this.email} />
+                </p>
+            </div>
+            <div className="field">
+                <p className="control">
+                 <textarea className="textarea" placeholder="รายละเอียด" onChange={this.detail}></textarea>
+                </p>
+            </div>
+            <a className="button is-danger" onClick={this.addData.bind(this, this.state)} >Upload</a>
+        </div>
         )
     }
 }
